2f430ffb6de9c8f08e07f8fd3ecf1b22286ddc7e
Natasha <natasha@deriv.com>
2024-08-12 17:17:35 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Header } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <h1 className="text-xl font-bold text-red-500 ">Hello Tailwind</h1>
+    </>
+  );
+};
+
+export default App;
